c501fba17649c147d97a8e895fdb598f2183e03e
Mohit-codex <kumarmohit9343@gmail.com>
2026-03-18 22:31:16 +0530

    i am adding java code on github

---
+++ third.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+class third {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the integer:");
+        int s = in.nextInt();
+
+        System.out.println("Your integer is " + s);
+
+        in.close(); 
+    }
+}
