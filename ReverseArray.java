59aa38defd3525294923971a3ff53bad12ee4c89
Mohit-codex <kumarmohit9343@gmail.com>
2026-03-26 23:21:32 +0530

    date-26 march 2026

---
+++ ReverseArray.java
@@ -0,0 +1,26 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        
+        System.out.println("Original Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        // Reverse logic
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+        System.out.println("\nReversed Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
